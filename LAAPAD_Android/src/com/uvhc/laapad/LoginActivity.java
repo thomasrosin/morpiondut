81fd6ba887cf806099856fa7f078e47037b0dfb4
Clément <Clment@PC-Zail>
2014-02-11 10:56:50 +0100

    [COMMIT]clean code

--- LAAPAD_Android/src/com/uvhc/laapad/LoginActivity.java
+++ LAAPAD_Android/src/com/uvhc/laapad/LoginActivity.java
@@ -1,19 +1,7 @@
 package com.uvhc.laapad;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
